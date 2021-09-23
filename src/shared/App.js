aaf04f37ccd0a2ddaf9fd3468d4b7e808863a951
senspond20 <senspond@gmail.com>
2021-09-24 06:46:27 +0900

    react router setting

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,26 +1,3 @@
-# React SSR
-
-Start
-
-```
-$ npm install -g yarn
-$ npx create-react-app .
-$ yarn add react-router-dom
-$ yarn add cross-env --dev
-```
-
-
-## React Router
-
-```
-$ yarn add query-string
-```
-
-> Sample-Code
-
-+ src/shared/App.js
-
-```js
 import React, { Component } from 'react';
 import { Route , Switch } from 'react-router-dom';
 import { Home, About } from '../pages';
@@ -45,27 +22,3 @@ class App extends Component {
 }
 
 export default App;
-```
-
-+ src/pages/About.js
-
-```js
-import React from 'react';
-import queryString from 'query-string';
-
-const About = ({location, match}) => {
-    const query = queryString.parse(location.search);
-
-    const detail = query.detail === 'true';
-
-    return (
-        <div>
-            <h2>About {match.params.name}</h2>
-            {detail && 'detail: http://localhost:3000/about?detail=true'}
-        </div>
-    );
-};
-
-export default About;
-```
-
