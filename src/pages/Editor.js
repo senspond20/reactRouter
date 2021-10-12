0e54f2557838128a05621d8c255fe23415caf0cb
senspond20 <senspond@gmail.com>
2021-10-12 14:52:14 +0900

    editor imageurl

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, {useRef, useEffect,useState} from 'react';
 import queryString from 'query-string';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
+
 const EditorPage = ({location, match}) => {
     const query = queryString.parse(location.search);
+    const [text,setText] = useState();
+    console.log(query);
+    const editorRef = useRef();
+
+    useEffect(() => {
+      if (editorRef.current) {
+        editorRef.current.getInstance().removeHook("addImageBlobHook");
+        editorRef.current
+          .getInstance()
+          .addHook("addImageBlobHook", (blob, callback) => {
+            (async () => {
+              let formData = new FormData();
+              formData.append("file", blob);
+  
+            //   axios.defaults.withCredentials = true;
+            //   const { data: url } = await axios.post(
+            //     `${backUrl}image.do`,
+            //     formData,
+            //     {
+            //       header: { "content-type": "multipart/formdata" },
+            //     }
+            //   );
+            //   callback(url, "alt text");
+            })();
+  
+            return false;
+          });
+      }
+  
+      return () => {};
+    }, [editorRef]);
 
     return (
         <div>
-            <h2>Editor</h2>
-            {/* ?detail=true 로 들어올때만 출력된다*/}
+            <h2>Editor</h2>    
             <Editor
                 initialValue="hello react editor world!"
                 previewStyle="vertical"
                 height="600px"
-                initialEditType="markdown"
+                initialEditType="wysiwyg"
                 useCommandShortcut={true}
+                onChange={() => {
+                  const innerTxt = editorRef.current.getInstance().getMarkdown();
+                  setText(innerTxt);
+                }}
+                ref={editorRef}
+            
             />
         </div>
     );
